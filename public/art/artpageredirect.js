d20aa62b310503fa3c73e71e883d4b5ee75197d4
Bossay <bossayadams@gmail.com>
2025-05-15 19:20:28 -0700

    Redirect fix

--- public/art/artpageredirect.js
+++ public/art/artpageredirect.js
@@ -1,5 +1,6 @@
 if (window.self == window.top) {
     // The page is not inside an iframe
     localStorage.setItem("redirectartPage", window.location.href);
-    window.location.href = "/art/art.html";
+    localStorage.setItem("redirectPage", url(/art/art.html));
+    window.location.href = "/index.html";
   }
